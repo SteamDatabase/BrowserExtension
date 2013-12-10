825cdc9aed1d1a6f0032a37d580476cb19747fc8
xPaw <github@xpaw.ru>
2013-12-10 16:34:59 +0200

    Remove returns and anonymous wrapper in app script

--- scripts/store/app.js
+++ scripts/store/app.js
@@ -1,64 +1,58 @@
-// TODO: Remove this wrapper, it's silly
-(function() {
-var container = document.getElementById( 'error_box' );
+var element, container = document.getElementById( 'error_box' );
 
 if( container )
 {
 	container.insertAdjacentHTML( 'beforeEnd', '<br><br><a target="_blank" href="'+ GetHomepage() + 'app/' + GetCurrentAppID() + '/">View on Steam Database</a>' );
-	
-	return;
 }
-
-container = document.querySelector( '#demo_block .block_content_inner' );
-
-if( !container )
+else
 {
-	return;
-}
-
-var element = document.createElement( 'div' );
-element.className = 'demo_area_button';
-element.innerHTML = '<a class="game_area_wishlist_btn steamdb_button" target="_blank" href="' + GetHomepage() + 'app/' + GetCurrentAppID() + '/">View on Steam Database</a>';
-
-container.insertBefore( element, container.firstChild );
-
-// Find each "add to cart" button
-container = document.querySelectorAll( 'input[name="subid"]' );
-
-var i = 0, subid = 0, length = container.length;
-
-for( i = 0; i < length; i++ )
-{
-	element = container[ i ];
+	container = document.querySelector( '#demo_block .block_content_inner' );
 	
-	subid = element.value;
-	
-	element = element.parentElement.parentElement;
-	
-	// Is this a subscription selector?
-	if( subid.length === 0 )
+	if( container )
 	{
-		if( element.querySelector( '.game_area_purchase_game_dropdown_selection' ) )
+		element = document.createElement( 'div' );
+		element.className = 'demo_area_button';
+		element.innerHTML = '<a class="game_area_wishlist_btn steamdb_button" target="_blank" href="' + GetHomepage() + 'app/' + GetCurrentAppID() + '/">View on Steam Database</a>';
+		
+		container.insertBefore( element, container.firstChild );
+	}
+	
+	// Find each "add to cart" button
+	container = document.querySelectorAll( 'input[name="subid"]' );
+	
+	var i = 0, subid = 0, length = container.length;
+	
+	for( i = 0; i < length; i++ )
+	{
+		element = container[ i ];
+		
+		subid = element.value;
+		
+		element = element.parentElement.parentElement;
+		
+		// Is this a subscription selector?
+		if( subid.length === 0 )
 		{
-			element.insertAdjacentHTML( 'beforeEnd', '<a class="steamdb_link' + ( element.querySelector( '.game_area_purchase_game_dropdown_left_panel' ) ? '' : ' steamdb_float_left' ) + '" target="_blank" href="'+ GetHomepage() + '">View on Steam Database <i class="steamdb_subid">(nothing selected)</i></a>' );
+			if( element.querySelector( '.game_area_purchase_game_dropdown_selection' ) )
+			{
+				element.insertAdjacentHTML( 'beforeEnd', '<a class="steamdb_link' + ( element.querySelector( '.game_area_purchase_game_dropdown_left_panel' ) ? '' : ' steamdb_float_left' ) + '" target="_blank" href="'+ GetHomepage() + '">View on Steam Database <i class="steamdb_subid">(nothing selected)</i></a>' );
+			}
+		}
+		else
+		{
+			element.insertAdjacentHTML( 'beforeEnd', '<a class="steamdb_link steamdb_float_left" target="_blank" href="'+ GetHomepage() + 'sub/' + subid + '/">View on Steam Database <i class="steamdb_subid">(' + subid + ')</i></a>' );
 		}
 	}
-	else
+	
+	// We have to inject our JS directly into the page to hook Steam's functionatily
+	if( document.querySelector( '.game_area_purchase_game_dropdown_selection' ) )
 	{
-		element.insertAdjacentHTML( 'beforeEnd', '<a class="steamdb_link steamdb_float_left" target="_blank" href="'+ GetHomepage() + 'sub/' + subid + '/">View on Steam Database <i class="steamdb_subid">(' + subid + ')</i></a>' );
+		element = document.createElement( 'script' );
+		element.id = 'steamdb_subscriptions_hook';
+		element.type = 'text/javascript';
+		element.src = chrome.extension.getURL( 'scripts/store/subscriptions.js' ); // TODO: abstract
+		element.dataset.homepage = GetHomepage();
+		
+		document.head.appendChild( element );
 	}
 }
-
-// We have to inject our JS directly into the page to hook Steam's functionatily
-if( document.querySelector( '.game_area_purchase_game_dropdown_selection' ) )
-{
-	element = document.createElement( 'script' );
-	element.id = 'steamdb_subscriptions_hook';
-	element.type = 'text/javascript';
-	element.src = chrome.extension.getURL( 'scripts/store/subscriptions.js' ); // TODO: abstract
-	element.dataset.homepage = GetHomepage();
-	
-	document.head.appendChild( element );
-}
-
-}());
