2d0d7e956546db633775f9d98606aa800d983dd3
xPaw <github@xpaw.ru>
2014-01-01 22:59:19 +0200

    Add links in dropdowns on games/wishlist pages

---
+++ scripts/community/profile_games.js
@@ -0,0 +1,32 @@
+var text,
+    link,
+    element,
+    i,
+    appID,
+    dropdowns = document.querySelectorAll( '[id^=links_dropdown_]' ),
+    length = dropdowns.length;
+
+for( i = 0; i < length; i++ )
+{
+	element = dropdowns[ i ];
+	
+	appID = element.id.replace( 'links_dropdown_', '' );
+	
+	element = element.querySelector( '.popup_body2' );
+	
+	if( element )
+	{
+		text = document.createElement( 'h5' );
+		text.appendChild( document.createTextNode( 'View on Steam Database' ) );
+		
+		link = document.createElement( 'a' );
+		link.className = 'popup_menu_item2 tight steamdb_button';
+		link.target = '_blank';
+		link.href = GetHomepage() + 'app/' + appID + '/';
+		link.appendChild( text );
+		
+		element.appendChild( link );
+	}
+}
+
+// TODO: Hook into the page and either edit gameLinksPopupTemplate or hook BuildGameRow function to handle UpdateChangingGames if it breaks
