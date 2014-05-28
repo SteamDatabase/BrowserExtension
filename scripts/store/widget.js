f66c267c37a188dacd9f6297bfffbc115a070c78
xPaw <github@xpaw.ru>
2014-05-28 15:23:13 +0300

    Add steamdb links to store widgets

---
+++ scripts/store/widget.js
@@ -0,0 +1,17 @@
+var subid = document.querySelector( 'input[name="subid"]' );
+
+if( subid )
+{
+	var subid = subid.value;
+	
+	var link = document.createElement( 'a' );
+	link.className = 'steamdb_link';
+	link.target = '_blank';
+	link.href = GetHomepage() + 'sub/' + subid + '/';
+	link.innerHTML = 'View on Steam Database <i class="steamdb_subid">(' + subid + ')</i>'; // TODO: fix
+	
+	var container = document.createElement( 'p' );
+	container.appendChild( link );
+	
+	document.querySelector( '.desc' ).appendChild( container );
+}
