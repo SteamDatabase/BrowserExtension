f79d7706b45f13308adbc3a9ac11c5624e1dbc2f
xPaw <github@xpaw.me>
2014-09-06 15:02:28 +0300

    Mozzzzzzillllllllaaaaaaaaaaa

--- firefox.sh
+++ firefox.sh
@@ -12,6 +12,7 @@ echo Copying files
 cp -r scripts/ styles/ images/ data/
 cp icons/18.png data/icons/18.png
 cp manifest.json data/manifest.json
+rm data/scripts/firefox.js
 
 echo Creating a package
 
