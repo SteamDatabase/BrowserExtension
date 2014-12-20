b94f08e915457b2c1cf8aaf621c90735d2bbc4ad
Tomáš Duda <tomas.duda1@gmail.com>
2014-12-20 18:30:07 +0100

    Checkbox Show wishlisted only on the sales page

---
+++ scripts/steamdb/sales.js
@@ -0,0 +1,71 @@
+'use strict';
+
+// Show wishlisted games
+
+var checkboxControl = document.createElement( 'div' );
+checkboxControl.className = 'steamy-checkbox-control';
+checkboxControl.id = 'js-wishlisted-only';
+
+var checkbox = document.createElement( 'div' );
+checkbox.className = 'steamy-checkbox';
+checkboxControl.appendChild( checkbox );
+
+var checkboxText = document.createElement( 'span' );
+checkboxText.className = 'steamy-checkbox-label';
+checkboxText.appendChild( document.createTextNode( 'Show wishlisted only' ) );
+checkboxControl.appendChild( checkboxText );
+
+var form = document.querySelector( '#currency-form' );
+form.classList.add( 'with-wishlist' );
+form.appendChild( checkboxControl );
+
+checkboxControl.addEventListener( 'click', function( ev )
+{
+	var showWishlistedOnly = checkboxControl.classList.toggle( 'checked' );
+	var hideOwned = document.querySelector( '#js-hide-owned-games.checked' );
+	
+	var elems = document.querySelectorAll( '.appimg' );
+	for ( var i = 0; i < elems.length; i++ )
+	{
+		if ( showWishlistedOnly )
+		{
+			if ( elems[ i ].classList.contains( 'wished' ) )
+			{
+				elems[ i ].hidden = false;
+			}
+			else
+			{
+				elems[ i ].hidden = true;
+			}
+		}
+		else
+		{
+			if ( hideOwned )
+			{
+				if ( ! elems[ i ].classList.contains( 'owned' ) )
+				{
+					elems[ i ].hidden = false;
+				}
+			}
+			else
+			{
+				elems[ i ].hidden = false;
+			}
+		}
+	}
+	
+	// Hide empty sections
+	
+	[].forEach.call( document.querySelectorAll( '.table-sales tbody' ), function( el )
+	{
+		var section = document.querySelector( '#sales-section-' + el.dataset.section );
+		if ( el.clientHeight == 0 && section.style.display != 'none' )
+		{
+			section.style.display = 'none';
+		}
+		else
+		{
+			section.style.display = '';
+		}
+	} );
+}, false);
