f9c525adcd60253be1d27f032077e47a7cba9577
Tomáš Duda <tomas.duda1@gmail.com>
2014-12-20 18:41:55 +0100

    Check if the highlight option is enabled

--- scripts/steamdb/sales.js
+++ scripts/steamdb/sales.js
@@ -2,63 +2,71 @@
 
 // Show wishlisted games
 
-var checkboxControl = document.createElement( 'div' );
-checkboxControl.className = 'steamy-checkbox-control';
-checkboxControl.id = 'js-wishlisted-only';
-
-var checkbox = document.createElement( 'div' );
-checkbox.className = 'steamy-checkbox';
-checkboxControl.appendChild( checkbox );
-
-var checkboxText = document.createElement( 'span' );
-checkboxText.className = 'steamy-checkbox-label';
-checkboxText.appendChild( document.createTextNode( 'Show wishlisted only' ) );
-checkboxControl.appendChild( checkboxText );
-
-var form = document.querySelector( '#currency-form' );
-form.classList.add( 'with-wishlist' );
-form.appendChild( checkboxControl );
-
-checkboxControl.addEventListener( 'click', function( ev )
+GetOption( { 'steamdb-highlight': true }, function( items )
 {
-	var showWishlistedOnly = checkboxControl.classList.toggle( 'checked' );
-	var hideOwned = document.querySelector( '#js-hide-owned-games.checked' );
-	
-	var elems = document.querySelectorAll( '.appimg' );
-	for ( var i = 0; i < elems.length; i++ )
+	if( !items[ 'steamdb-highlight' ] )
 	{
-		if ( showWishlistedOnly )
+		return;
+	}
+
+	var checkboxControl = document.createElement( 'div' );
+	checkboxControl.className = 'steamy-checkbox-control';
+	checkboxControl.id = 'js-wishlisted-only';
+
+	var checkbox = document.createElement( 'div' );
+	checkbox.className = 'steamy-checkbox';
+	checkboxControl.appendChild( checkbox );
+
+	var checkboxText = document.createElement( 'span' );
+	checkboxText.className = 'steamy-checkbox-label';
+	checkboxText.appendChild( document.createTextNode( 'Show wishlisted only' ) );
+	checkboxControl.appendChild( checkboxText );
+
+	var form = document.querySelector( '#currency-form' );
+	form.classList.add( 'with-wishlist' );
+	form.appendChild( checkboxControl );
+
+	checkboxControl.addEventListener( 'click', function( ev )
+	{
+		var showWishlistedOnly = checkboxControl.classList.toggle( 'checked' );
+		var hideOwned = document.querySelector( '#js-hide-owned-games.checked' );
+		
+		var elems = document.querySelectorAll( '.appimg' );
+		for ( var i = 0; i < elems.length; i++ )
 		{
-			if ( elems[ i ].classList.contains( 'wished' ) )
+			if ( showWishlistedOnly )
 			{
-				elems[ i ].hidden = false;
-			}
-			else
-			{
-				elems[ i ].hidden = true;
-			}
-		}
-		else
-		{
-			if ( hideOwned )
-			{
-				if ( ! elems[ i ].classList.contains( 'owned' ) )
+				if ( elems[ i ].classList.contains( 'wished' ) )
 				{
 					elems[ i ].hidden = false;
+				}
+				else
+				{
+					elems[ i ].hidden = true;
 				}
 			}
 			else
 			{
-				elems[ i ].hidden = false;
+				if ( hideOwned )
+				{
+					if ( ! elems[ i ].classList.contains( 'owned' ) )
+					{
+						elems[ i ].hidden = false;
+					}
+				}
+				else
+				{
+					elems[ i ].hidden = false;
+				}
 			}
 		}
-	}
-	
-	// Hide empty sections
-	
-	[].forEach.call( document.querySelectorAll( '.table-sales tbody' ), function( el )
-	{
-		var section = document.querySelector( '#sales-section-' + el.dataset.section );
-		section.hidden = ( el.clientHeight == 0 && ! section.hidden );
-	} );
-}, false);
+		
+		// Hide empty sections
+		
+		[].forEach.call( document.querySelectorAll( '.table-sales tbody' ), function( el )
+		{
+			var section = document.querySelector( '#sales-section-' + el.dataset.section );
+			section.hidden = ( el.clientHeight == 0 && ! section.hidden );
+		} );
+	}, false);
+} );
