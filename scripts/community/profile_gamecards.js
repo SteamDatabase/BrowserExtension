d7b784ee280b411e8f462092e5af20d0d0e411aa
xPaw <github@xpaw.me>
2015-01-24 12:55:17 +0200

    Add steamdb and store page buttons on game cards page

---
+++ scripts/community/profile_gamecards.js
@@ -0,0 +1,62 @@
+'use strict';
+
+GetOption( { 'button-gamecards': true }, function( items )
+{
+	if( !items[ 'button-gamecards' ] )
+	{
+		return;
+	}
+	
+	var profileTexture = document.querySelector( '.profile_small_header_texture' );
+	
+	if( !profileTexture )
+	{
+		return;
+	}
+	
+	// Container
+	var container = document.createElement( 'div' );
+	container.className = 'profile_small_header_additional steamdb';
+	
+	// Store button
+	var span = document.createElement( 'span' );
+	span.appendChild( document.createTextNode( 'Store Page' ) );
+	
+	var link = document.createElement( 'a' );
+	link.className = 'btnv6_blue_hoverfade btn_medium';
+	link.href = '//store.steampowered.com/app/' + GetCurrentAppID() + '/';
+	link.appendChild( span );
+	
+	container.insertBefore( link, container.firstChild );
+	
+	// SteamDB button
+	var image = document.createElement( 'img' );
+	image.className = 'ico16';
+	image.src = GetLocalResource( 'icons/white.svg' );
+	
+	span = document.createElement( 'span' );
+	span.dataset.communityTooltip = 'View on Steam Database';
+	span.appendChild( image );
+	
+	link = document.createElement( 'a' );
+	link.className = 'btnv6_blue_hoverfade btn_medium btn_steamdb';
+	link.target = '_blank';
+	link.href = GetHomepage() + 'app/' + GetCurrentAppID() + '/';
+	link.appendChild( span );
+	
+	container.insertBefore( link, container.firstChild );
+	
+	// Best hacks EU
+	container.insertBefore( document.createTextNode( ' ' ), link.nextSibling );
+	
+	// Add to the page
+	profileTexture.appendChild( container );
+	
+	// Tooltip hack
+	element = document.createElement( 'script' );
+	element.id = 'steamdb_bind_tooltip';
+	element.type = 'text/javascript';
+	element.appendChild( document.createTextNode( 'BindCommunityTooltip( jQuery( ".btn_steamdb > span" ) );' ) );
+	
+	document.head.appendChild( element );
+} );
