49e0d2c21c6c3f4b0a88d9b33f7edbad9a9314ac
xPaw <github@xpaw.me>
2015-01-28 20:00:16 +0200

    Direct package link if we are able to get it from "remove" button
    Fixes #236

--- scripts/store/account.js
+++ scripts/store/account.js
@@ -12,7 +12,8 @@ GetOption( { 'link-accountpage': true }, function( items )
 	var licenses = document.querySelectorAll( '.account_table tr > td:first-child' ),
 	    link,
 	    title,
-	    element;
+	    element,
+	    removeElement;
 	
 	if( licenses )
 	{
@@ -22,8 +23,36 @@ GetOption( { 'link-accountpage': true }, function( items )
 			title = element.textContent;
 			
 			link = document.createElement( 'a' );
+			
+			// nextSibling won't work due to whitespace
+			removeElement = element.parentNode.querySelector( 'td:nth-child(2) > a' );
+			
+			if( removeElement )
+			{
+				if( !title.length )
+				{
+					title = '?? no package name ??';
+					
+					element.appendChild( document.createTextNode( title ) );
+				}
+				
+				removeElement = removeElement.href.match( /RemoveFreeLicense\( ?([0-9]+)/ );
+				
+				console.log( removeElement );
+				
+				link.href = GetHomepage() + 'sub/' + ( removeElement ? removeElement[ 1 ] : removeElement ) + '/';
+			}
+			// Valve somehow managed not to put package name on the page
+			else if( !title.length )
+			{
+				continue;
+			}
+			else
+			{
+				link.href = GetHomepage() + 'search/?a=sub&q=' + encodeURIComponent( title );
+			}
+			
 			link.target = '_blank';
-			link.href = GetHomepage() + 'search/?a=sub&q=' + encodeURIComponent( title );
 			link.appendChild( document.createTextNode( title ) );
 			
 			element.replaceChild( link, element.firstChild );
