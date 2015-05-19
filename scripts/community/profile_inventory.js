cf63af1e635304ddeea29e61f97a520f4c8c518b
xPaw <github@xpaw.me>
2015-05-19 19:53:11 +0300

    Add an option to push inventory game list to the side

--- scripts/community/profile_inventory.js
+++ scripts/community/profile_inventory.js
@@ -2,24 +2,33 @@
 
 if( document.getElementById( 'inventory_link_753' ) )
 {
-	GetOption( { 'link-inventory': true, 'link-inventory-gift-subid': true }, function( items )
+	GetOption( { 'link-inventory': true, 'link-inventory-gift-subid': true, 'enhancement-inventory-sidebar': true }, function( items )
 	{
-		if( !items[ 'link-inventory' ] )
+		if( items[ 'enhancement-inventory-sidebar' ] )
 		{
-			return;
+			var style = document.createElement( 'link' );
+			style.id = 'steamdb_inventory_sidebar';
+			style.type = 'text/css';
+			style.rel = 'stylesheet';
+			style.href = GetLocalResource( 'styles/inventory-sidebar.css' );
+			
+			document.head.appendChild( style );
 		}
 		
-		if( items[ 'link-inventory-gift-subid' ] )
+		if( items[ 'link-inventory' ] )
 		{
-			document.body.dataset.steamdbGiftSubid = 'true';
+			if( items[ 'link-inventory-gift-subid' ] )
+			{
+				document.body.dataset.steamdbGiftSubid = 'true';
+			}
+			
+			var element = document.createElement( 'script' );
+			element.id = 'steamdb_inventory_hook';
+			element.type = 'text/javascript';
+			element.src = GetLocalResource( 'scripts/community/inventory.js' );
+			element.dataset.homepage = GetHomepage();
+			
+			document.head.appendChild( element );
 		}
-		
-		var element = document.createElement( 'script' );
-		element.id = 'steamdb_inventory_hook';
-		element.type = 'text/javascript';
-		element.src = GetLocalResource( 'scripts/community/inventory.js' );
-		element.dataset.homepage = GetHomepage();
-		
-		document.head.appendChild( element );
 	} );
 }
