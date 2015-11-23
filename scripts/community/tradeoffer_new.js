f234a4969edcccf970583e944c309e4a64992062
Pavel Djundik <github@xpaw.me>
2015-11-24 00:39:22 +0200

    Add support for for_item param in tradeoffers

---
+++ scripts/community/tradeoffer_new.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var item = window.location.search.match( /[\?&]for_item=([0-9]+)_([0-9]+)_([0-9]+)/ );
+
+if( item !== null )
+{
+	var InjectScript = function( appid, contextid, assetid )
+	{
+		window.g_rgCurrentTradeStatus =
+		{
+			"newversion": true,
+			"version": 1,
+			"me": {
+			    "assets": [],
+			    "currency": [],
+			    "ready": false
+			},
+			"them": {
+			    "assets": [
+			        {
+			            "appid": appid,
+			            "contextid": contextid,
+			            "assetid": assetid,
+			            "amount": 1
+			        }
+			    ],
+			    "currency": [],
+			    "ready": false
+			}
+		};
+		
+		window.RedrawCurrentTradeStatus();
+	};
+	
+	var element = document.createElement( 'script' );
+	element.id = 'steamdb_for_item';
+	element.type = 'text/javascript';
+	element.appendChild( document.createTextNode( '(' + InjectScript.toString() + '(' + item[ 1 ] + ',' + item[ 2 ] + ',' + item[ 3 ] + '))') );
+	
+	document.head.appendChild( element );
+}
