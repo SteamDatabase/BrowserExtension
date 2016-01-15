e2beac59db25b3aec57906cd8ef46c212641c335
Alexander Corn <mckay@doctormckay.com>
2016-01-15 10:58:51 -0500

    Fixed "offer changed" in trade offers

--- scripts/community/tradeoffer_new.js
+++ scripts/community/tradeoffer_new.js
@@ -39,3 +39,21 @@ if( item !== null )
 	
 	document.head.appendChild( element );
 }
+
+// Fix "offer changed"
+var FixTradeOffer = function ()
+{
+	var originalToggleReady = window.ToggleReady;
+	window.ToggleReady = function ( ready )
+	{
+		window.g_rgCurrentTradeStatus.me.ready = ready;
+		originalToggleReady.apply( this, arguments );
+	};
+};
+
+var element = document.createElement( 'script' );
+element.id = 'steamdb_fix_tradeoffers';
+element.type = 'text/javascript';
+element.appendChild (document.createTextNode ( '(' + FixTradeOffer.toString() + '())') );
+
+document.head.appendChild( element );
