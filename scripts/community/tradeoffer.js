e9a44e552886ba871ac296bc8dba87d18ea39bab
Pavel Djundik <github@xpaw.me>
2016-06-24 22:16:26 +0300

    Remove duplicate explanation

--- scripts/community/tradeoffer.js
+++ scripts/community/tradeoffer.js
@@ -75,7 +75,6 @@ var FixTradeOffer = function()
 				case 20: explanation = 'The trade offer server is temporarily unavailable.'; break;
 				case 25: explanation = 'You cannot send this trade offer because you have exceeded your active offer limit.<br><br>You are limited to 5 outstanding trade offers to a single user, and 30 outstanding trade offers in total.'; break;
 				case 26: explanation = 'One or more of the items in this trade offer is no longer present in the inventory from which it is being requested.<br><br>Please check all items to ensure that they still exist and are tradable.'; break;
-				case 50: explanation = 'One or more of the items in this trade offer is no longer present in the inventory from which it is being requested.<br><br>Please check all items to ensure that they still exist and are tradable.'; break;
 			}
 			
 			if( explanation )
