6840a148dc7b4fb646e7f5492d64bac6236cc970
Pavel Djundik <github@xpaw.me>
2017-02-25 12:12:40 +0200

    Fix MS Edge compatability

--- scripts/common.js
+++ scripts/common.js
@@ -3,51 +3,57 @@
 
 'use strict';
 
-var CurrentAppID,
-	GetAppIDFromUrl = function( url )
-	{
-		var appid = url.match( /\/(app|sub|bundle|friendsthatplay|gamecards|recommended)\/([0-9]{1,7})/ );
-		
-		return appid ? parseInt( appid[ 2 ], 10 ) : -1;
-	},
+var IsEdgeBrowser = GetLocalResource( 'manifest.json' ).indexOf( 'ms-browser-extension://' ) === 0;
+var CurrentAppID;
+
+function GetAppIDFromUrl( url )
+{
+	var appid = url.match( /\/(app|sub|bundle|friendsthatplay|gamecards|recommended)\/([0-9]{1,7})/ );
 	
-	GetCurrentAppID = function()
+	return appid ? parseInt( appid[ 2 ], 10 ) : -1;
+}
+
+function GetCurrentAppID()
+{
+	if( !CurrentAppID )
 	{
-		if( !CurrentAppID )
-		{
-			CurrentAppID = GetAppIDFromUrl( location.pathname );
-		}
-		
-		return CurrentAppID;
-	},
+		CurrentAppID = GetAppIDFromUrl( location.pathname );
+	}
 	
-	GetHomepage = function()
+	return CurrentAppID;
+}
+
+function GetHomepage()
+{
+	return 'https://steamdb.info/';
+}
+
+function GetOption( items, callback )
+{
+	if( typeof chrome !== 'undefined' && typeof chrome.storage !== 'undefined' )
 	{
-		return 'https://steamdb.info/';
-	},
+		chrome.storage.local.get( items, callback );
+	}
+	else if( IsEdgeBrowser )
+	{
+		browser.storage.local.get( items, callback );
+	}
+	else if( typeof browser !== 'undefined' )
+	{
+		browser.storage.local.get( items ).then( callback );
+	}
+}
+
+function GetLocalResource( res )
+{
+	if( typeof chrome !== 'undefined' && typeof chrome.extension !== 'undefined' )
+	{
+		return chrome.extension.getURL( res );
+	}
+	else if( typeof browser !== 'undefined' )
+	{
+		return browser.extension.getURL( res );
+	}
 	
-	GetOption = function( items, callback )
-	{
-		if( typeof chrome !== 'undefined' )
-		{
-			chrome.storage.local.get( items, callback );
-		}
-		else if( typeof browser !== 'undefined' )
-		{
-			browser.storage.local.get( items ).then( callback );
-		}
-	},
-	
-	GetLocalResource = function( res )
-	{
-		if( typeof chrome !== 'undefined' )
-		{
-			return chrome.extension.getURL( res );
-		}
-		else if( typeof browser !== 'undefined' )
-		{
-			return browser.extension.getURL( res );
-		}
-		
-		return res;
-	};
+	return res;
+}
