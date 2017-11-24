0ddb84e9f1ce84f2dee6b48a3a4cce9d32435add
Pavel Djundik <github@xpaw.me>
2017-11-24 13:19:38 +0200

    Lint

--- scripts/https-fix.js
+++ scripts/https-fix.js
@@ -51,9 +51,12 @@
 	// Hook all XHR requests and fix https
 	const originalXhrOpen = XMLHttpRequest.prototype.open;
 	
-	XMLHttpRequest.prototype.open = function( method, url )
+	XMLHttpRequest.prototype.open = function( )
 	{
-		arguments[ 1 ] = arguments[ 1 ].replace( /^http:/, 'https:' );
+		if( arguments.length > 1 )
+		{
+			arguments[ 1 ] = arguments[ 1 ].replace( /^http:/, 'https:' );
+		}
 		
 		return originalXhrOpen.apply( this, arguments );
 	};
