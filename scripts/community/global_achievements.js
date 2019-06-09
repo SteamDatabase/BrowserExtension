c7f7d19c20ea6a502f35afc9091fea489ce6682a
Pavel Djundik <xPaw@users.noreply.github.com>
2019-06-09 14:12:16 +0300

    Fix achievements url

--- scripts/community/global_achievements.js
+++ scripts/community/global_achievements.js
@@ -1,22 +1,18 @@
 'use strict';
 
-var compareAvatar = document.querySelector( '#compareAvatar a' );
+const compareAvatar = document.querySelector( '#compareAvatar a' );
+const path = window.location.pathname.match( /^\/stats\/\w+/ );
 
-if( compareAvatar )
+if( compareAvatar && path )
 {
-	const path = window.location.pathname.match( /^\/stats\/\w+/ );
+	const tab = document.createElement( 'div' );
+	tab.className = 'tab steamdb_stats_tab';
 
-	if( path )
-	{
-		const tab = document.createElement( 'div' );
-		tab.className = 'tab steamdb_stats_tab';
+	const link = document.createElement( 'a' );
+	link.className = 'tabOn';
+	link.href = `${compareAvatar.href}${path}?tab=achievements`;
+	link.textContent = 'View your achievements';
 
-		const link = document.createElement( 'a' );
-		link.className = 'tabOn';
-		link.href = path + '?tab=achievements';
-		link.textContent = 'View your achievements';
-
-		tab.appendChild( link );
-		document.querySelector( '#tabs' ).appendChild( tab );
-	}
+	tab.appendChild( link );
+	document.querySelector( '#tabs' ).appendChild( tab );
 }
