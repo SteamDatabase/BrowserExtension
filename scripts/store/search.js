30dabf3a51b64138607f8234938059ded69a455a
Revadike <revadike@outlook.com>
2019-09-02 04:05:47 +0200

    Added some whitelines

--- scripts/store/search.js
+++ scripts/store/search.js
@@ -8,7 +8,7 @@ GetOption( { 'button-search': true }, function( items )
 	}
 	
 	var container = document.querySelector( '#advsearchform .rightcol' );
-	
+
 	if( !container )
 	{
         return;
@@ -18,9 +18,11 @@ GetOption( { 'button-search': true }, function( items )
     {
         var link = 'https://steamdb.info/search/?';
         var params = decodeURIComponent( window.location.search );
-        var types = { 999: 8, 998: 1, 997: 1, 994: 2, 10: 3, 21: 4, 992: 13, 993: 16 };
+
         var HasParam = ( param ) => params.includes( param + '=' );
         var GetParam = ( param ) => HasParam( param ) && encodeURIComponent( params.split( param + '=' )[ 1 ].split( /&|#/ )[ 0 ].split( ',' )[ 0 ] );
+
+        var types = { 999: 8, 998: 1, 997: 1, 994: 2, 10: 3, 21: 4, 992: 13, 993: 16 };
         var typeparam = HasParam( 'category1' ) && types[ GetParam( 'category1' ) ] ? '&type=' + types[ GetParam( 'category1' ) ] : '';
 
         switch( true )
@@ -28,33 +30,43 @@ GetOption( { 'button-search': true }, function( items )
             case HasParam( 'filter' ) && GetParam( 'filter' ) === 'comingsoon':
                 link = 'https://steamdb.info/upcoming/';
                 break;
+
             case HasParam( 'category2' ):
                 link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'category2' ) + typeparam;
                 break;
+
             case HasParam( 'category3' ):
                 link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'category3' ) + typeparam;
                 break;
+
             case HasParam( 'vrsupport' ):
                 link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'vrsupport' ) + typeparam;
                 break;
+
             case HasParam( 'term' ):
                 link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=0' + typeparam;
                 break;
+
             case HasParam( 'os' ):
                 link += 'a=app_keynames&keyname=92&operator=1&keyvalue=' + GetParam( 'os' ) + typeparam;
                 break;
+
             case HasParam( 'developer' ):
                 link += 'a=app_keynames&keyname=23&operator=3&keyvalue=' + GetParam( 'developer' ) + typeparam;
                 break;
+
             case HasParam( 'pubisher' ):
                 link += 'a=app_keynames&keyname=241&operator=3&keyvalue=' + GetParam( 'pubisher' ) + typeparam;
                 break;
+
             case HasParam( 'supportedlang' ):
                 link += 'a=app_keynames&keyname=444&operator=1&keyvalue=' + GetParam( 'supportedlang' ) + typeparam;
                 break;
+
             default:
                 link += 'a=app&q=&category=0' + typeparam;
                 break;
+
         }
 
         return link + '&utm_source=Steam&utm_medium=Steam&utm_campaign=SteamDB%20Extension';
@@ -68,6 +80,6 @@ GetOption( { 'button-search': true }, function( items )
     button.style.width = '100%';
     button.addEventListener( 'click', () => window.open( GetSteamDBLink(), '_blank' ) );
     button.appendChild( span );
-    
+
     container.insertBefore( button, container.firstChild );
 } );
