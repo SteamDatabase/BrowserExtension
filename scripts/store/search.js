73dd09b03f252e94ee020010daf7fa3e6c855768
Revadike <revadike@outlook.com>
2019-09-02 03:53:27 +0200

    Button now gets steam link when onclick

--- scripts/store/search.js
+++ scripts/store/search.js
@@ -9,67 +9,64 @@ GetOption( { 'button-search': true }, function( items )
 	
 	var container = document.querySelector( '#advsearchform .rightcol' );
 	
-	if( container )
+	if( !container )
 	{
-        var GetSteamDBLink = function()
-        {
-            var link = 'https://steamdb.info/search/?';
-            var params = window.location.search;
-            var types = { 999: 8, 998: 1, 997: 1, 994: 2, 10: 3, 21: 4, 992: 13, 993: 16 };
-            var HasParam = ( param ) => params.includes( param + '=' );
-            var GetParam = ( param ) => HasParam( param ) && params.split( param + '=' )[ 1 ].split( /&|#/ )[ 0 ];
-            var typeparam = HasParam( 'category1' ) && types[ GetParam( 'category1' ) ] ? '&type=' + types[ GetParam( 'category1' ) ] : '';
-    
-            switch( true )
-            {
-                case HasParam( 'filter' ) && GetParam( 'filter' ) === 'comingsoon':
-                    link = 'https://steamdb.info/upcoming/';
-                    break;
-                case HasParam( 'category2' ):
-                    link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'category2' ) + typeparam;
-                    break;
-                case HasParam( 'category3' ):
-                    link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'category3' ) + typeparam;
-                    break;
-                case HasParam( 'vrsupport' ):
-                    link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'vrsupport' ) + typeparam;
-                    break;
-                case HasParam( 'term' ):
-                    link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=0' + typeparam;
-                    break;
-                case HasParam( 'os' ):
-                    link += 'a=app_keynames&keyname=92&operator=1&keyvalue=' + GetParam( 'os' ) + typeparam;
-                    break;
-                case HasParam( 'developer' ):
-                    link += 'a=app_keynames&keyname=23&operator=3&keyvalue=' + GetParam( 'developer' ) + typeparam;
-                    break;
-                case HasParam( 'pubisher' ):
-                    link += 'a=app_keynames&keyname=241&operator=3&keyvalue=' + GetParam( 'pubisher' ) + typeparam;
-                    break;
-                case HasParam( 'supportedlang' ):
-                    link += 'a=app_keynames&keyname=444&operator=1&keyvalue=' + GetParam( 'supportedlang' ) + typeparam;
-                    break;
-                default:
-                    link += 'a=app&q=&category=0' + typeparam;
-                    break;
-            }
-    
-            return link + '&utm_source=Steam&utm_medium=Steam&utm_campaign=SteamDB%20Extension';
-        };
+        return;
+    }
 
-		var element = document.createElement( 'span' );
-		element.appendChild( document.createTextNode( 'View on Steam Database' ) );
-		
-		var link = document.createElement( 'a' );
-		link.rel = 'noopener';
-		link.className = 'action_btn';
-		link.href = GetSteamDBLink();
-		link.appendChild( element );
-		
-		element = document.createElement( 'button' );
-		element.className = 'btnv6_blue_hoverfade btn_small';
-		element.appendChild( link );
-		
-		container.insertBefore( element, container.firstChild );
-	}
+    var GetSteamDBLink = function()
+    {
+        var link = 'https://steamdb.info/search/?';
+        var params = decodeURIComponent( window.location.search );
+        var types = { 999: 8, 998: 1, 997: 1, 994: 2, 10: 3, 21: 4, 992: 13, 993: 16 };
+        var HasParam = ( param ) => params.includes( param + '=' );
+        var GetParam = ( param ) => HasParam( param ) && params.split( param + '=' )[ 1 ].split( /&|#/ )[ 0 ].split( ',' )[ 0 ];
+        var typeparam = HasParam( 'category1' ) && types[ GetParam( 'category1' ) ] ? '&type=' + types[ GetParam( 'category1' ) ] : '';
+
+        switch( true )
+        {
+            case HasParam( 'filter' ) && GetParam( 'filter' ) === 'comingsoon':
+                link = 'https://steamdb.info/upcoming/';
+                break;
+            case HasParam( 'category2' ):
+                link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'category2' ) + typeparam;
+                break;
+            case HasParam( 'category3' ):
+                link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'category3' ) + typeparam;
+                break;
+            case HasParam( 'vrsupport' ):
+                link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=' + GetParam( 'vrsupport' ) + typeparam;
+                break;
+            case HasParam( 'term' ):
+                link += 'a=app&q=' + encodeURIComponent( GetParam( 'term' ) || '' ) + '&category=0' + typeparam;
+                break;
+            case HasParam( 'os' ):
+                link += 'a=app_keynames&keyname=92&operator=1&keyvalue=' + GetParam( 'os' ) + typeparam;
+                break;
+            case HasParam( 'developer' ):
+                link += 'a=app_keynames&keyname=23&operator=3&keyvalue=' + GetParam( 'developer' ) + typeparam;
+                break;
+            case HasParam( 'pubisher' ):
+                link += 'a=app_keynames&keyname=241&operator=3&keyvalue=' + GetParam( 'pubisher' ) + typeparam;
+                break;
+            case HasParam( 'supportedlang' ):
+                link += 'a=app_keynames&keyname=444&operator=1&keyvalue=' + GetParam( 'supportedlang' ) + typeparam;
+                break;
+            default:
+                link += 'a=app&q=&category=0' + typeparam;
+                break;
+        }
+
+        return link + '&utm_source=Steam&utm_medium=Steam&utm_campaign=SteamDB%20Extension';
+    };
+
+    var span = document.createElement( 'span' );
+    span.appendChild( document.createTextNode( 'View on Steam Database' ) );
+    
+    var button = document.createElement( 'button' );
+    button.className = 'btnv6_blue_hoverfade btn_small';
+    button.addEventListener( 'click', () => window.open( GetSteamDBLink(), '_blank' ) );
+    button.appendChild( span );
+    
+    container.insertBefore( button, container.firstChild );
 } );
