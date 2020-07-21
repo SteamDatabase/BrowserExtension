cac7728d59b70d0ece3b61084d6d500024136c8a
Pavel Djundik <xPaw@users.noreply.github.com>
2020-07-21 23:31:19 +0300

    Fix subids in subscription dropdowns

--- scripts/store/subscriptions.js
+++ scripts/store/subscriptions.js
@@ -1,26 +1,23 @@
 ( function()
 {
 	'use strict';
-	
-	let link;
+
 	const homepage = document.getElementById( 'steamdb_subscriptions_hook' ).dataset.homepage;
-	const originalDropdownSelectOption = window.dropdownSelectOption;
-	
-	window.dropdownSelectOption = function( dropdownName, subId )
+	const originalDropdownSelectOption = window.GamePurchaseDropdownSelectOption;
+
+	window.GamePurchaseDropdownSelectOption = function SteamDB_GamePurchaseDropdownSelectOption( dropdownName, subId )
 	{
-		try
-		{
-			link = document.getElementById( 'add_to_cart_' + dropdownName + '_description_text' );
-			link = link.parentNode.querySelector( '.steamdb_link' );
-			
-			link.href = homepage + 'sub/' + subId + '/?utm_source=Steam&utm_medium=Steam&utm_campaign=SteamDB%20Extension';
-			link.querySelector( '.steamdb_subid' ).textContent = 'Sub ' + subId;
-		}
-		catch( e )
-		{
-			// Don't break website functionality if something fails above
-		}
-		
 		originalDropdownSelectOption.apply( this, arguments );
+
+		let link = document.getElementById( `add_to_cart_${dropdownName}` );
+
+		if( !link )
+		{
+			return;
+		}
+
+		link = link.parentNode.querySelector( '.steamdb_link' );
+		link.href = `${homepage}sub/${subId}/?utm_source=Steam&utm_medium=Steam&utm_campaign=SteamDB%20Extension`;
+		link.querySelector( '.steamdb_subid' ).textContent = `Sub ${subId}`;
 	};
 }() );
