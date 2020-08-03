3dd9248f2261402989d34717d2e3fa69ffd0c315
Pavel Djundik <xPaw@users.noreply.github.com>
2020-08-03 13:27:48 +0300

    Add an option to hide hidden achievements behind a spoiler tag

--- scripts/community/global_achievements.js
+++ scripts/community/global_achievements.js
@@ -1,62 +1,93 @@
 'use strict';
 
-const compareAvatar = document.querySelector( '#compareAvatar a' );
-const path = window.location.pathname.match( /^\/stats\/\w+/ );
-
-if( compareAvatar && path )
+GetOption( {
+	'spoiler-achievements': true,
+}, function( items )
 {
-	const tab = document.createElement( 'div' );
-	tab.className = 'tab steamdb_stats_tab';
+	const spoilerAchievements = !!items[ 'spoiler-achievements' ];
+	const compareAvatar = document.querySelector( '#compareAvatar a' );
+	const path = window.location.pathname.match( /^\/stats\/\w+/ );
 
-	const link = document.createElement( 'a' );
-	link.className = 'tabOn';
-	link.href = `${compareAvatar.href}${path}?tab=achievements`;
-	link.textContent = 'View your achievements';
+	if( compareAvatar && path )
+	{
+		const tab = document.createElement( 'div' );
+		tab.className = 'tab steamdb_stats_tab';
 
-	tab.appendChild( link );
-	document.querySelector( '#tabs' ).appendChild( tab );
+		const link = document.createElement( 'a' );
+		link.className = 'tabOn';
+		link.href = `${compareAvatar.href}${path}?tab=achievements`;
+		link.textContent = 'View your achievements';
 
-	const headers = new Headers();
-	headers.append( 'X-ValveUserAgent', 'panorama' );
+		tab.appendChild( link );
+		document.querySelector( '#tabs' ).appendChild( tab );
 
-	fetch( compareAvatar.href + path + '?tab=achievements&panorama=please', {
-		headers: headers,
-	} )
-		.then( ( response ) => response.text() )
-		.then( ( response ) =>
-		{
-			response = response.match( /g_rgAchievements\s*=\s*(\{.+?\});/ );
-		
-			if( !response )
+		const headers = new Headers();
+		headers.append( 'X-ValveUserAgent', 'panorama' );
+
+		fetch( compareAvatar.href + path + '?tab=achievements&panorama=please', {
+			headers: headers,
+		} )
+			.then( ( response ) => response.text() )
+			.then( ( response ) =>
 			{
-				return;
-			}
-		
-			response = JSON.parse( response[ 1 ] );
-		
-			if( !response.open )
-			{
-				return;
-			}
+				response = response.match( /g_rgAchievements\s*=\s*(\{.+?\});/ );
 
-			const elements = document.querySelectorAll( '.achieveTxt > h3' );
-
-			for( const key in response.open )
-			{
-				const achievement = response.open[ key ];
-
-				if( !achievement.hidden || achievement.closed )
+				if( !response )
 				{
-					continue;
+					return;
 				}
-				for( let i = 0; i < elements.length; i++ )
+
+				response = JSON.parse( response[ 1 ] );
+
+				if( !response.open )
 				{
-					if( elements[ i ].textContent === achievement.name )
+					return;
+				}
+
+				const elements = document.querySelectorAll( '.achieveTxt > h3' );
+
+				for( const key in response.open )
+				{
+					const achievement = response.open[ key ];
+
+					if( !achievement.hidden || achievement.closed )
 					{
-						elements[ i ].parentNode.querySelector( 'h5' ).textContent = `[HIDDEN] ${achievement.desc}`;
+						continue;
+					}
+
+					for( const element of elements )
+					{
+						if( element.textContent !== achievement.name )
+						{
+							continue;
+						}
+
+						const parent = element.parentNode.querySelector( 'h5' );
+
+						if( spoilerAchievements )
+						{
+							const span = document.createElement( 'span' );
+							span.className = 'steamdb_achievement_spoiler';
+							span.appendChild( document.createTextNode( achievement.desc ) );
+
+							const hiddenAchiev = document.createElement( 'i' );
+							hiddenAchiev.textContent = 'Hidden achievement, mouse over to reveal: ';
+
+							parent.appendChild( hiddenAchiev );
+							parent.appendChild( span );
+						}
+						else
+						{
+							const hiddenAchiev = document.createElement( 'i' );
+							hiddenAchiev.textContent = 'Hidden achievement: ';
+
+							parent.appendChild( hiddenAchiev );
+							parent.appendChild( document.createTextNode( achievement.desc ) );
+						}
+
 						break;
 					}
 				}
-			}
-		} );
-}
+			} );
+	}
+} );
--- scripts/community/profile_achievements.js
+++ scripts/community/profile_achievements.js
@@ -1,73 +1,99 @@
 'use strict';
 
-let achievementsContainer = document.querySelector( '.achieveHiddenBox' );
-
-if( achievementsContainer )
+GetOption( {
+	'spoiler-achievements': true,
+}, function( items )
 {
-	achievementsContainer = achievementsContainer.parentNode.parentNode;
-	
-	const headers = new Headers();
-	headers.append( 'X-ValveUserAgent', 'panorama' );
+	const spoilerAchievements = !!items[ 'spoiler-achievements' ];
+	let achievementsContainer = document.querySelector( '.achieveHiddenBox' );
 
-	fetch( window.location.origin + window.location.pathname + '?tab=achievements&panorama=please', {
-		headers: headers,
-	} )
-		.then( ( response ) => response.text() )
-		.then( ( response ) =>
-		{
-			response = response.match( /g_rgAchievements\s*=\s*(\{.+?\});/ );
-		
-			if( !response )
+	if( achievementsContainer )
+	{
+		achievementsContainer = achievementsContainer.parentNode.parentNode;
+
+		const headers = new Headers();
+		headers.append( 'X-ValveUserAgent', 'panorama' );
+
+		fetch( window.location.origin + window.location.pathname + '?tab=achievements&panorama=please', {
+			headers: headers,
+		} )
+			.then( ( response ) => response.text() )
+			.then( ( response ) =>
 			{
-				return;
-			}
-		
-			response = JSON.parse( response[ 1 ] );
-		
-			if( !response.open )
-			{
-				return;
-			}
-		
-			for( const key in response.open )
-			{
-				const achievement = response.open[ key ];
-			
-				if( !achievement.hidden || achievement.closed )
+				response = response.match( /g_rgAchievements\s*=\s*(\{.+?\});/ );
+
+				if( !response )
 				{
-					continue;
+					return;
 				}
-			
-				const image = document.createElement( 'img' );
-				image.src = achievement.icon_open;
-			
-				const achieveImgHolder = document.createElement( 'div' );
-				achieveImgHolder.className = 'achieveImgHolder';
-				achieveImgHolder.appendChild( image );
-			
-				const h3 = document.createElement( 'h3' );
-				h3.className = 'ellipsis';
-				h3.appendChild( document.createTextNode( achievement.name ) );
-			
-				const h5 = document.createElement( 'h5' );
-				h5.className = 'ellipsis';
-				h5.appendChild( document.createTextNode( `[HIDDEN] ${achievement.desc}` ) );
-			
-				const achieveTxt = document.createElement( 'div' );
-				achieveTxt.className = 'achieveTxt';
-				achieveTxt.appendChild( h3 );
-				achieveTxt.appendChild( h5 );
-			
-				const achieveTxtHolder = document.createElement( 'div' );
-				achieveTxtHolder.className = 'achieveTxtHolder';
-				achieveTxtHolder.appendChild( achieveTxt );
-			
-				const achieveRow = document.createElement( 'div' );
-				achieveRow.className = 'achieveRow';
-				achieveRow.appendChild( achieveImgHolder );
-				achieveRow.appendChild( achieveTxtHolder );
-			
-				achievementsContainer.appendChild( achieveRow );
-			}
-		} );
-}
+
+				response = JSON.parse( response[ 1 ] );
+
+				if( !response.open )
+				{
+					return;
+				}
+
+				for( const key in response.open )
+				{
+					const achievement = response.open[ key ];
+
+					if( !achievement.hidden || achievement.closed )
+					{
+						continue;
+					}
+
+					const image = document.createElement( 'img' );
+					image.src = achievement.icon_open;
+
+					const achieveImgHolder = document.createElement( 'div' );
+					achieveImgHolder.className = 'achieveImgHolder';
+					achieveImgHolder.appendChild( image );
+
+					const h3 = document.createElement( 'h3' );
+					h3.className = 'ellipsis';
+					h3.appendChild( document.createTextNode( achievement.name ) );
+
+					const h5 = document.createElement( 'h5' );
+					h5.className = 'ellipsis';
+
+					if( spoilerAchievements )
+					{
+						const span = document.createElement( 'span' );
+						span.className = 'steamdb_achievement_spoiler';
+						span.appendChild( document.createTextNode( achievement.desc ) );
+
+						const hiddenAchiev = document.createElement( 'i' );
+						hiddenAchiev.textContent = 'Hidden achievement, mouse over to reveal: ';
+
+						h5.appendChild( hiddenAchiev );
+						h5.appendChild( span );
+					}
+					else
+					{
+						const hiddenAchiev = document.createElement( 'i' );
+						hiddenAchiev.textContent = 'Hidden achievement: ';
+
+						h5.appendChild( hiddenAchiev );
+						h5.appendChild( document.createTextNode( achievement.desc ) );
+					}
+
+					const achieveTxt = document.createElement( 'div' );
+					achieveTxt.className = 'achieveTxt';
+					achieveTxt.appendChild( h3 );
+					achieveTxt.appendChild( h5 );
+
+					const achieveTxtHolder = document.createElement( 'div' );
+					achieveTxtHolder.className = 'achieveTxtHolder';
+					achieveTxtHolder.appendChild( achieveTxt );
+
+					const achieveRow = document.createElement( 'div' );
+					achieveRow.className = 'achieveRow';
+					achieveRow.appendChild( achieveImgHolder );
+					achieveRow.appendChild( achieveTxtHolder );
+
+					achievementsContainer.appendChild( achieveRow );
+				}
+			} );
+	}
+} );
