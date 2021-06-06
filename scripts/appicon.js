442ab93d60fb995bc4656804409b703229ec482f
Pavel Djundik <xPaw@users.noreply.github.com>
2021-06-06 18:43:51 +0300

    Fix %CDN_HOST_MEDIA_SSL% in app icon

--- scripts/appicon.js
+++ scripts/appicon.js
@@ -13,5 +13,32 @@ GetOption( {
 		style.href = GetLocalResource( 'styles/appicon.css' );
 
 		document.head.appendChild( style );
+
+		// Fix cdn url
+		window.addEventListener( 'DOMContentLoaded', () =>
+		{
+			const icon = document.querySelector( '.apphub_AppIcon > img' );
+
+			if( !icon )
+			{
+				return;
+			}
+
+			if( !icon.src.includes( '%CDN_HOST_MEDIA_SSL%' ) )
+			{
+				return;
+			}
+
+			const metaImage = document.querySelector( 'meta[itemprop="image"]' );
+
+			if( !metaImage )
+			{
+				return;
+			}
+
+			const url = new URL( document.querySelector( 'link[rel="image_src"]' ).href );
+
+			icon.src = icon.src.replace( '%CDN_HOST_MEDIA_SSL%', url.host );
+		} );
 	}
 } );
