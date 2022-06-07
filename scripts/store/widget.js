e30024dc6f1b8868ee69792b2ba04e3eded344ef
Pavel Djundik <xPaw@users.noreply.github.com>
2022-06-07 11:32:45 +0300

    Translate widget

--- scripts/store/widget.js
+++ scripts/store/widget.js
@@ -17,14 +17,14 @@ GetOption( { 'link-subid-widget': true }, function( items )
 	if( subid )
 	{
 		link.href = GetHomepage() + 'sub/' + subid.value + '/?utm_source=Steam&utm_medium=Steam&utm_campaign=SteamDB%20Extension';
-		link.textContent = `View sub ${subid.value} on SteamDB`;
+		link.textContent = _t( 'widget_view_sub', subid.value );
 	}
 	else
 	{
 		const appid = GetCurrentAppID();
 
 		link.href = GetHomepage() + 'app/' + appid + '/?utm_source=Steam&utm_medium=Steam&utm_campaign=SteamDB%20Extension';
-		link.textContent = `View app ${appid} on SteamDB`;
+		link.textContent = _t( 'widget_view_app', appid );
 	}
 
 	const container = document.createElement( 'p' );
