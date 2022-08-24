cebd28dc588f7a2c550c4194745693a1b90653c0
Pavel Djundik <xPaw@users.noreply.github.com>
2022-08-24 23:24:43 +0300

    Make card drops counter work with any language

--- scripts/community/profile_badges.js
+++ scripts/community/profile_badges.js
@@ -10,11 +10,11 @@ if( progressInfo.length > 0 )
 
 	for( let i = 0; i < progressInfo.length; i++ )
 	{
-		match = progressInfo[ i ].textContent.match( /([0-9]+) card drops? remaining/ );
+		match = progressInfo[ i ].textContent.match( /(?<number>[0-9]+)/ );
 
 		if( match )
 		{
-			match = parseInt( match[ 1 ], 10 ) || 0;
+			match = parseInt( match.groups.number, 10 ) || 0;
 
 			if( match > 0 )
 			{
