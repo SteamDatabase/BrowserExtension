02c1f7cbae102aac7e9e85dcb859fdacab80c405
Pavel Djundik <xPaw@users.noreply.github.com>
2023-08-12 19:39:59 +0300

    Pack separate zip for firefox

--- build.js
+++ build.js
@@ -2,36 +2,83 @@ const fs = require( 'fs' );
 const path = require( 'path' );
 const archiver = require( 'archiver' );
 const manifest = require( './manifest.json' );
+const version = manifest.version.replace( /\./g, '_' );
 
-const zipPath = path.join( __dirname, `steamdb_ext_${manifest.version.replace( /\./g, '_' )}.zip` );
-
-console.log( `Packaging to ${zipPath}` );
-
-const output = fs.createWriteStream( zipPath );
-const archive = archiver( 'zip' );
-
-output.on( 'close', () =>
+( async() =>
 {
-	console.log( archive.pointer() + ' total bytes' );
-} );
+	await ArchiveChromium();
+	await ArchiveFirefox();
+} )();
 
-archive.on( 'warning', err =>
+function ArchiveChromium()
 {
-	throw err;
-} );
+	const zipPath = path.join( __dirname, `steamdb_ext_${version}.zip` );
+	const archive = PrepareArchive( zipPath );
 
-archive.on( 'error', err =>
+	archive.file( path.join( __dirname, 'manifest.json' ), { name: 'manifest.json' } );
+	return archive.finalize();
+}
+
+function ArchiveFirefox()
 {
-	throw err;
-} );
+	const zipPath = path.join( __dirname, `steamdb_ext_${version}_firefox.zip` );
+	const archive = PrepareArchive( zipPath );
 
-archive.pipe( output );
+	const ffManifest = manifest;
+	ffManifest.background =
+	{
+		scripts:
+		[
+			'scripts/background.js',
+		],
+	};
+	ffManifest.browser_specific_settings =
+	{
+		gecko:
+		{
+			id: 'firefox-extension@steamdb.info',
+			strict_min_version: '105.0',
+		},
+	};
 
-archive.file( path.join( __dirname, 'LICENSE' ), { name: 'LICENSE' } );
-archive.file( path.join( __dirname, 'manifest.json' ), { name: 'manifest.json' } );
-archive.directory( path.join( __dirname, 'icons/' ), 'icons' );
-archive.directory( path.join( __dirname, 'options/' ), 'options' );
-archive.directory( path.join( __dirname, 'scripts/' ), 'scripts' );
-archive.directory( path.join( __dirname, 'styles/' ), 'styles' );
-archive.directory( path.join( __dirname, '_locales/' ), '_locales' );
-archive.finalize();
+	delete ffManifest.web_accessible_resources[ 0 ].use_dynamic_url; // Unsupported
+
+	const json = JSON.stringify( ffManifest, null, '\t' );
+
+	archive.append( json, { name: 'manifest.json' } );
+	return archive.finalize();
+}
+
+function PrepareArchive( zipPath )
+{
+	console.log( `Packaging to ${zipPath}` );
+
+	const output = fs.createWriteStream( zipPath );
+	const archive = archiver( 'zip' );
+
+	output.on( 'close', () =>
+	{
+		console.log( `Written ${archive.pointer()} total bytes to ${zipPath}` );
+	} );
+
+	archive.on( 'warning', err =>
+	{
+		throw err;
+	} );
+
+	archive.on( 'error', err =>
+	{
+		throw err;
+	} );
+
+	archive.pipe( output );
+
+	archive.file( path.join( __dirname, 'LICENSE' ), { name: 'LICENSE' } );
+	archive.directory( path.join( __dirname, 'icons/' ), 'icons' );
+	archive.directory( path.join( __dirname, 'options/' ), 'options' );
+	archive.directory( path.join( __dirname, 'scripts/' ), 'scripts' );
+	archive.directory( path.join( __dirname, 'styles/' ), 'styles' );
+	archive.directory( path.join( __dirname, '_locales/' ), '_locales' );
+
+	return archive;
+}
