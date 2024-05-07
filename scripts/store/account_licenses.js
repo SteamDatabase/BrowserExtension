1acd4435acd96fd682550bf5c3a129de74fea0fe
Pavel Djundik <xPaw@users.noreply.github.com>
2024-05-07 12:56:12 +0300

    Optimize loading licenses page

---
+++ scripts/store/account_licenses.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const script = document.createElement( 'script' );
+script.id = 'steamdb_disable_tooltips';
+script.type = 'text/javascript';
+script.src = GetLocalResource( 'scripts/store/account_licenses_injected.js' );
+document.documentElement.append( script );
+
+GetOption( { 'link-accountpage': true }, function( items )
+{
+	const addLinks = items[ 'link-accountpage' ];
+
+	if( document.readyState === 'loading' )
+	{
+		document.addEventListener( 'DOMContentLoaded', OnContentLoaded );
+	}
+	else
+	{
+		OnContentLoaded();
+	}
+
+	function OnContentLoaded()
+	{
+		const table = document.querySelector( '.account_table' );
+
+		if( !addLinks || !table )
+		{
+			document.body.classList.add( 'steamdb_account_table_loaded' );
+			return;
+		}
+
+		const licenses = table.querySelectorAll( 'tr' );
+
+		if( licenses )
+		{
+			const params = new URLSearchParams();
+			params.set( 'a', 'sub' );
+			params.set( 'q', '' );
+			params.set( 'utm_source', 'Steam' );
+			params.set( 'utm_medium', 'Steam' );
+			params.set( 'utm_campaign', 'SteamDB Extension' );
+
+			for( const tr of licenses )
+			{
+				const nameCell = tr.cells[ 1 ];
+
+				if( nameCell.tagName === 'TH' )
+				{
+					const newTd = document.createElement( 'th' );
+					newTd.className = 'steamdb_license_id_col';
+					newTd.textContent = 'SteamDB';
+					nameCell.after( newTd );
+
+					continue;
+				}
+
+				const link = document.createElement( 'a' );
+				const removeElement = nameCell.querySelector( '.free_license_remove_link a' );
+
+				if( removeElement )
+				{
+					const subidMatch = removeElement.href.match( /RemoveFreeLicense\( ?(?<subid>[0-9]+)/ );
+
+					if( !subidMatch )
+					{
+						continue;
+					}
+
+					const subid = subidMatch.groups.subid;
+
+					link.href = `${GetHomepage()}sub/${subid}/?utm_source=Steam&utm_medium=Steam&utm_campaign=SteamDB%20Extension`;
+					link.textContent = subid;
+				}
+				else
+				{
+					params.set( 'q', nameCell.textContent.trim() );
+
+					link.href = `${GetHomepage()}search/?${params.toString()}`;
+					link.textContent = 'Search';
+				}
+
+				const newTd = document.createElement( 'td' );
+				newTd.className = 'steamdb_license_id_col';
+				newTd.append( link );
+				nameCell.after( newTd );
+			}
+		}
+
+		document.body.classList.add( 'steamdb_account_table_loaded' );
+	}
+} );
---
+++ scripts/store/account_licenses_injected.js
@@ -0,0 +1,49 @@
+( function()
+{
+	'use strict';
+
+	if( document.body )
+	{
+		PerformHook();
+	}
+	else
+	{
+		// If the script was injected too early, wait for <body> element to be created
+		const observer = new MutationObserver( () =>
+		{
+			if( document.body )
+			{
+				PerformHook();
+
+				observer.disconnect();
+			}
+		} );
+
+		observer.observe( document, {
+			childList: true,
+			subtree: true,
+		} );
+	}
+
+	function PerformHook()
+	{
+		const noop = () =>
+		{
+			// noop
+		};
+
+		window.InstrumentLinks = noop;
+		window.BindTooltips = noop;
+
+		if( window.GDynamicStore )
+		{
+			window.GDynamicStore.Init = noop;
+		}
+
+		// As Valve's own comment says this function is for "perf sensitive pages"
+		if( window.DisableTooltipMutationObserver )
+		{
+			window.DisableTooltipMutationObserver();
+		}
+	}
+}() );
