023b4834ba4783d09a12b651ed36f6fd0e9523dc
Pavel Djundik <xPaw@users.noreply.github.com>
2024-07-10 22:14:17 +0300

    Fix steamdb links on bundle/sub agecheck pages, hide pcgw

--- scripts/store/app_error.js
+++ scripts/store/app_error.js
@@ -5,13 +5,22 @@
 // eslint-disable-next-line no-unused-vars
 function AddLinksInErrorBox( container )
 {
+	const pageTypeMatch = location.pathname.match( /^\/(?:[a-z]+\/)?(?<type>app|bundle|sub)\// );
+
+	if( !pageTypeMatch )
+	{
+		return;
+	}
+
+	const pageType = pageTypeMatch.groups.type;
+
 	const linkContainer = document.createElement( 'div' );
 	linkContainer.className = 'steamdb_error_link';
 
 	// SteamDB
 	let link = document.createElement( 'a' );
 	link.className = 'btnv6_blue_hoverfade btn_medium';
-	link.href = GetHomepage() + 'app/' + GetCurrentAppID() + '/';
+	link.href = `${GetHomepage()}${pageType}/${GetCurrentAppID()}/`;
 
 	let text = document.createElement( 'span' );
 	text.append( document.createTextNode( _t( 'view_on_steamdb' ) ) );
@@ -19,18 +28,21 @@ function AddLinksInErrorBox( container )
 
 	linkContainer.append( link );
 
-	linkContainer.append( document.createTextNode( ' ' ) );
-
 	// PCGW
-	link = document.createElement( 'a' );
-	link.className = 'btnv6_blue_hoverfade btn_medium';
-	link.href = 'https://pcgamingwiki.com/api/appid.php?appid=' + GetCurrentAppID() + '&utm_source=SteamDB';
+	if( pageType === 'app' )
+	{
+		linkContainer.append( document.createTextNode( ' ' ) );
 
-	text = document.createElement( 'span' );
-	text.append( document.createTextNode( _t( 'view_on_pcgamingwiki' ) ) );
-	link.append( text );
+		link = document.createElement( 'a' );
+		link.className = 'btnv6_blue_hoverfade btn_medium';
+		link.href = 'https://pcgamingwiki.com/api/appid.php?appid=' + GetCurrentAppID() + '&utm_source=SteamDB';
 
-	linkContainer.append( link );
+		text = document.createElement( 'span' );
+		text.append( document.createTextNode( _t( 'view_on_pcgamingwiki' ) ) );
+		link.append( text );
+
+		linkContainer.append( link );
+	}
 
 	container.append( linkContainer );
 }
