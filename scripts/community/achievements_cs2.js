ac5d52e26076db7ba77465e939a3f85ad00e311f
Геннадий <geku94@gmail.com>
2024-09-09 02:44:18 +0300

    Add chart, some layout changes to the table

--- scripts/community/achievements_cs2.js
+++ scripts/community/achievements_cs2.js
@@ -2,26 +2,163 @@
 
 'use strict';
 
+const DrawChart = ( initialData, hoveredIndex = -1, canvas = null, tooltip = null ) =>
+{
+	const maxLength = 200;
+	if( !canvas )
+	{
+		canvas = document.createElement( 'canvas' );
+		canvas.className = 'steamdb_achievements_csrating_graph';
+		document.querySelector( '#mainContents' ).append( canvas );
+
+		const tooltip = document.createElement( 'div' );
+		tooltip.className = 'steamdb_achievements_csrating_graph_tooltip';
+		document.body.append( tooltip );
+
+		canvas.addEventListener( 'mousemove', ( event ) =>
+		{
+			const gap = canvas.offsetWidth / ( Math.min( initialData.length, maxLength ) );
+			const x = event.offsetX - ( gap / 2 );
+			const index = Math.ceil( x / gap );
+			DrawChart( initialData, index, canvas, tooltip );
+			tooltip.style.display = 'block';
+
+			const tooltipWidth = tooltip.clientWidth;
+			const shiftTooltip = event.pageX + tooltipWidth - document.body.clientWidth;
+			tooltip.style.left = shiftTooltip > 0
+				? event.pageX - shiftTooltip + 'px'
+				: event.pageX + 'px';
+			tooltip.style.top = event.pageY + 30 + 'px';
+		} );
+
+		const resetCanvas = () =>
+		{
+			DrawChart( initialData, -1, canvas, tooltip );
+			tooltip.style.display = 'none';
+		};
+		canvas.addEventListener( 'mouseleave', resetCanvas );
+		window.addEventListener( 'resize', resetCanvas );
+	}
+
+	canvas.width = canvas.parentElement.clientWidth;
+
+	const data = initialData.slice( 0, maxLength ).reverse();
+	const points = data.map( d => d.csr );
+	const maxCSR = Math.max( ...data.map( p => p.csr ) );
+
+	const rect = canvas.getBoundingClientRect();
+	const width = rect.width * devicePixelRatio;
+	const height = rect.height * devicePixelRatio;
+
+	const ctx = canvas.getContext( '2d' );
+
+	if( !points || ctx === null )
+	{
+		return;
+	}
+
+	// Setting size clears the canvas
+	canvas.width = width;
+	canvas.height = height;
+
+	// Draw gradient
+	const grd = ctx.createLinearGradient( 0, 0, 0, height );
+	grd.addColorStop( 0, 'rgba(93, 145, 223, .2)' );
+	grd.addColorStop( 1, 'transparent' );
+
+	ctx.fillStyle = grd;
+
+	ctx.beginPath();
+
+	let i = 0;
+	const paddedHeight = height * 0.95;
+	const halfHeight = height / 2;
+	const gap = width / ( points.length - 1 );
+
+	ctx.moveTo( 0, height );
+
+	for( const point of points )
+	{
+		const val = 2 * ( point / maxCSR - 0.5 );
+		const x = i * gap;
+		const y = ( -val * paddedHeight ) / 2 + halfHeight;
+		ctx.lineTo( x, y );
+
+		i += 1;
+	}
+
+	ctx.lineTo( width, height );
+	ctx.fill();
+
+	ctx.beginPath();
+
+	// Draw line
+	ctx.strokeStyle = '#5d91df';
+	ctx.lineWidth = 2 * devicePixelRatio;
+
+	let circleX = null;
+	let circleY = null;
+	let highlightedCSR = '';
+	let highlightedDate = '';
+	i = 0;
+
+	for( const point of points )
+	{
+		const val = 2 * ( point / maxCSR - 0.5 );
+		const x = i * gap;
+		const y = ( -val * paddedHeight ) / 2 + halfHeight;
+
+		if( i === 0 )
+		{
+			ctx.moveTo( x, y );
+		}
+		else
+		{
+			ctx.lineTo( x, y );
+		}
+
+		if( hoveredIndex === i )
+		{
+			circleX = x;
+			circleY = y;
+			highlightedCSR = data[ i ].csr;
+			highlightedDate = data[ i ].datetime;
+		}
+
+		i += 1;
+	}
+	ctx.stroke();
+
+	if( circleX !== null && circleY !== null )
+	{
+		ctx.beginPath();
+		ctx.fillStyle = '#fff';
+		ctx.arc( circleX, circleY, 3 * devicePixelRatio, 0, Math.PI * 2 );
+		ctx.fill();
+		tooltip.textContent = `${highlightedCSR.toLocaleString( 'en-US' )}\n${highlightedDate}`;
+	}
+};
+
 const CreateCSRatingTable = ( rows ) =>
 {
 	const table = document.createElement( 'table' );
 	table.className = 'steamdb_achievements_csrating';
+	document.querySelector( '#mainContents' ).append( table );
 
-	const thead = document.createElement( 'thead' );
-	table.append( thead );
+	const CreateHeader = () =>
+	{
+		const header = document.createElement( 'tr' );
 
-	const header = document.createElement( 'tr' );
-	thead.append( header );
+		const headerTdDatetime = document.createElement( 'th' );
+		headerTdDatetime.textContent = 'Updated';
+		const headerTdCSR = document.createElement( 'th' );
+		headerTdCSR.textContent = 'CS Rating';
+		const headerTdCSRdelta = document.createElement( 'th' );
+		headerTdCSRdelta.textContent = 'Δ';
+		header.append( headerTdDatetime, headerTdCSR, headerTdCSRdelta );
 
-	const headerTdSeason = document.createElement( 'th' );
-	headerTdSeason.textContent = 'Season';
-	const headerTdDatetime = document.createElement( 'th' );
-	headerTdDatetime.textContent = 'Updated';
-	const headerTdCSR = document.createElement( 'th' );
-	headerTdCSR.textContent = 'CS Rating';
-	const headerTdCSRdelta = document.createElement( 'th' );
-	headerTdCSRdelta.textContent = 'Δ';
-	header.append( headerTdSeason, headerTdDatetime, headerTdCSR, headerTdCSRdelta );
+		return header;
+	};
 
 	let prevScore = 0;
 	for( let i = rows.length - 1; i >= 0; i-- )
@@ -33,45 +170,55 @@ const CreateCSRatingTable = ( rows ) =>
 		prevScore = rows[ i ].csr;
 	}
 
-	const dateFormatter = new Intl.DateTimeFormat( GetLanguage(), {
-		dateStyle: 'medium',
-		timeStyle: 'short',
-	} );
-
 	const tbody = document.createElement( 'tbody' );
 	table.append( tbody );
 
+	let season;
+
 	for( const row of rows )
 	{
+		if( season !== row.season )
+		{
+			season = row.season;
+			const tr = document.createElement( 'tr' );
+			tbody.append( tr );
+
+			const th = document.createElement( 'th' );
+			th.textContent = `Season ${season}`;
+			th.colSpan = '3';
+			th.className = 'steamdb_achievements_csrating_season';
+			tr.append( th );
+
+			tbody.append( CreateHeader() );
+		}
+
 		const tr = document.createElement( 'tr' );
 		tbody.append( tr );
 
-		const season = document.createElement( 'td' );
-		season.textContent = `Season ${row.season}`;
-		tr.append( season );
-
 		const datetime = document.createElement( 'td' );
-		datetime.textContent = dateFormatter.format( new Date( row.datetime ).getTime() );
+		datetime.textContent = row.datetime;
 		tr.append( datetime );
 
 		const csr = document.createElement( 'td' );
-		csr.textContent = row.csr;
+		csr.textContent = row.csr.toLocaleString( 'en-US' );
 		const tier = Math.floor( row.csr / 5000 );
 		csr.className = `steamdb_achievements_csrating-value steamdb_achievements_csrating-tier${tier}`;
 		tr.append( csr );
 
 		const delta = document.createElement( 'td' );
-		delta.textContent = row.delta;
+		if( row.delta )
+		{
+			delta.textContent = ( row.delta > 0 ? '+' : '' ) + row.delta;
+		}
 		delta.className = row.delta > 0
 			? 'steamdb_achievements_csrating_positive'
 			: 'steamdb_achievements_csrating_negative';
+		if( row.delta < -199 || row.delta > 199 )
+		{
+			delta.classList.add( 'steamdb_achievements_csrating_significant' );
+		}
 		tr.append( delta );
 	}
-
-	StartViewTransition( () =>
-	{
-		document.querySelector( '#mainContents' ).append( table );
-	} );
 };
 
 const FetchCSRating = async() =>
@@ -85,19 +232,30 @@ const FetchCSRating = async() =>
 	const rows = [ ...dom.querySelectorAll( 'tr' ) ]
 		.filter( tr => tr.querySelector( 'td' )?.textContent.startsWith( 'premier' ) );
 
+	const dateFormatter = new Intl.DateTimeFormat( GetLanguage(), {
+		dateStyle: 'medium',
+		timeStyle: 'short',
+	} );
+
 	const premierRows = [];
 	for( const row of rows )
 	{
 		premierRows.push( {
 			season: row.querySelector( 'td' ).textContent.replace( 'premier_season', '' ),
-			datetime: row.querySelector( 'td:nth-child(2)' ).textContent,
+			datetime: dateFormatter.format(
+				new Date( row.querySelector( 'td:nth-child(2)' ).textContent ).getTime(),
+			),
 			csr: Number( row.querySelector( 'td:nth-child(3)' ).textContent ) >> 15,
 		} );
 	}
 
 	if( premierRows.length )
 	{
-		CreateCSRatingTable( premierRows );
+		StartViewTransition( () =>
+		{
+			DrawChart( premierRows );
+			CreateCSRatingTable( premierRows );
+		} );
 	}
 };
 
