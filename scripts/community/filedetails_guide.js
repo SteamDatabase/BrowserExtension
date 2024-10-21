1fbf54c513880572879cec1bad30d4c551b7a94b
Геннадий <geku94@gmail.com>
2024-10-21 14:13:01 +0300

    Use startViewTransition, show checkbox, don't keep spoiler list in memory

--- scripts/community/filedetails_guide.js
+++ scripts/community/filedetails_guide.js
@@ -3,10 +3,31 @@
 if( document.querySelector( '.guideTopContent' ) )
 {
 	const guide = document.querySelector( '.guide' );
-	const spoilers = guide.querySelectorAll( '.bb_spoiler' );
 
-	if( spoilers.length > 0 )
+	if( guide.querySelector( '.bb_spoiler' ) )
 	{
+		const StartViewTransition = ( callback ) =>
+		{
+			if( document.startViewTransition )
+			{
+				document.startViewTransition( () =>
+				{
+					try
+					{
+						callback();
+					}
+					catch( e )
+					{
+						console.error( e );
+					}
+				} );
+			}
+			else
+			{
+				callback();
+			}
+		};
+
 		const controls = document.querySelector( '#ItemControls' );
 
 		const divider = document.createElement( 'div' );
@@ -16,25 +37,32 @@ if( document.querySelector( '.guideTopContent' ) )
 		const checkboxWrapper = document.createElement( 'label' );
 		checkboxWrapper.textContent = _t( 'spoilers_reveal' );
 		checkboxWrapper.className = 'workshopItemControlCtn general_btn';
+		checkboxWrapper.style.gap = '5px';
 
 		const checkbox = document.createElement( 'input' );
 		checkbox.type = 'checkbox';
-		checkbox.style.display = 'none';
+		checkbox.style.colorScheme = 'dark';
+		checkbox.style.margin = 0;
+		checkbox.style.cursor = 'pointer';
 		checkboxWrapper.prepend( checkbox );
 		controls.append( checkboxWrapper );
 
 		checkbox.addEventListener( 'change', event =>
 		{
+			const spoilers = guide.querySelectorAll( '.bb_spoiler,.steamdb_spoiler_revealed' );
 			const reveal = event.target.checked;
 
 			checkboxWrapper.style.backgroundColor = reveal ? '#2d6bcd' : '';
 			checkboxWrapper.style.color = reveal ? '#fff' : '';
 
-			for( const spoiler of spoilers )
+			StartViewTransition( () =>
 			{
-				spoiler.classList.toggle( 'steamdb_spoiler_revealed', reveal );
-				spoiler.classList.toggle( 'bb_spoiler', !reveal );
-			}
+				for( const spoiler of spoilers )
+				{
+					spoiler.classList.toggle( 'steamdb_spoiler_revealed', reveal );
+					spoiler.classList.toggle( 'bb_spoiler', !reveal );
+				}
+			} );
 		} );
 	}
 }
