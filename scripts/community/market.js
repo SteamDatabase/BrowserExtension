3f566cb6c328571817645e3b82b52ff47669baa4
Pavel Djundik <xPaw@users.noreply.github.com>
2024-12-03 21:22:37 +0200

    Remove the enhancement_market_stickers option since Valve just shipped it natively

--- scripts/community/market.js
+++ scripts/community/market.js
@@ -5,9 +5,3 @@ script.id = 'steamdb_market_script';
 script.type = 'text/javascript';
 script.src = GetLocalResource( 'scripts/community/market_injected.js' );
 document.documentElement.append( script );
-
-GetOption( { 'enhancement-market-stickers': true }, ( items ) =>
-{
-	// Hopes and prayers that this setting loads before the DOM finishes
-	script.dataset.stickers = !!items[ 'enhancement-market-stickers' ];
-} );
