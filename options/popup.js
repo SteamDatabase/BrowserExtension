2cd897d897f024511f3d62de4d995382d4c01f46
Pavel Djundik <xPaw@users.noreply.github.com>
2025-03-30 13:00:08 +0300

    Add action popup

---
+++ options/popup.js
@@ -0,0 +1,23 @@
+'use strict';
+
+( () =>
+{
+	const browserApi = typeof browser !== 'undefined' ? browser : chrome;
+
+	document.body.dir = browserApi.i18n.getMessage( '@@bidi_dir' );
+
+	/** @type {NodeListOf<HTMLElement>} */
+	const localizable = document.querySelectorAll( '[data-msg]' );
+
+	for( const element of localizable )
+	{
+		element.innerHTML = browserApi.i18n.getMessage( element.dataset.msg );
+	}
+
+	document.getElementById( 'options-link' ).addEventListener( 'click', function( ev )
+	{
+		ev.preventDefault();
+
+		browserApi.runtime.openOptionsPage();
+	} );
+} )();
