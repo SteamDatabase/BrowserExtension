0246412981a417ceab1cf1099c735c5141264fd0
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2025-07-19 11:04:25 +0200

    feat: display available date for packs on boostercreator (#228)

---
+++ scripts/community/boostercreator_injected.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {HTMLSelectElement} */
+const gameSelector = document.querySelector( '#booster_game_selector' );
+
+if( gameSelector )
+{
+	// Add an event listener to catch when the game selector changes and emit new rgBoosterData
+	gameSelector.addEventListener( 'change', emitBoosterAvailableDate );
+
+	// Emit rgBoosterData for current selection when the script loads
+	emitBoosterAvailableDate();
+}
+
+function emitBoosterAvailableDate( )
+{
+	const selectedGame = gameSelector.value;
+	const rgBoosterData = window.CBoosterCreatorPage.sm_rgBoosterData[ selectedGame ];
+
+	gameSelector.dispatchEvent( new CustomEvent( 'steamdb-booster-game-change', { detail: rgBoosterData } ) );
+}
