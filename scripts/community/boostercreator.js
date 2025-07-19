bffc567d8696008054a66baaac875d8eefd8573c
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2025-07-19 10:54:09 +0200

    chore: return earlier when possible

--- scripts/community/boostercreator.js
+++ scripts/community/boostercreator.js
@@ -1,10 +1,15 @@
 'use strict';
 
-/** @type {HTMLSelectElement} */
-const gameSelector = document.querySelector( '#booster_game_selector' );
-
-if( gameSelector )
+( ( () =>
 {
+	/** @type {HTMLSelectElement} */
+	const gameSelector = document.querySelector( '#booster_game_selector' );
+
+	if( !gameSelector )
+	{
+		return;
+	}
+
 	// Add a `div` container to display the booster pack available date
 	const availableDateContainer = document.createElement( 'div' );
 	availableDateContainer.id = 'booster_available_date';
@@ -18,28 +23,27 @@ if( gameSelector )
 		const customEvent = /** @type {CustomEvent} */ ( event );
 		displayBoosterAvailableDate( customEvent.detail?.available_at_time );
 	} );
-}
 
-/**
- * @param {string | undefined} availableDate
- */
-function displayBoosterAvailableDate( availableDate )
-{
-	const availableDateContainer = document.getElementById( 'booster_available_date' );
 
-	if( availableDate )
+	/**
+	 * @param {string | undefined} availableDate
+	 */
+	function displayBoosterAvailableDate( availableDate )
 	{
-		availableDateContainer.textContent = _t( 'boostercreator_available_at_date', [ availableDate ] );
+		if( availableDate )
+		{
+			availableDateContainer.textContent = _t( 'boostercreator_available_at_date', [ availableDate ] );
+		}
+		else
+		{
+			availableDateContainer.textContent = '';
+		}
 	}
-	else
-	{
- 		availableDateContainer.textContent = '';
-	}
-}
 
-// Inject a script into the page, so we can access page Steam variables
-const script = document.createElement( 'script' );
-script.id = 'steamdb_boostercreator';
-script.type = 'text/javascript';
-script.src = GetLocalResource( 'scripts/community/boostercreator_injected.js' );
-document.head.appendChild( script );
+	// Inject a script into the page, so we can access page Steam variables
+	const script = document.createElement( 'script' );
+	script.id = 'steamdb_boostercreator';
+	script.type = 'text/javascript';
+	script.src = GetLocalResource( 'scripts/community/boostercreator_injected.js' );
+	document.head.appendChild( script );
+} )() );
