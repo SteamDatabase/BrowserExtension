d137df0627d726724b6c8bb4c171a0e8b1bf4868
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2025-07-19 10:20:33 +0200

    feat: localize shown text

--- scripts/community/boostercreator.js
+++ scripts/community/boostercreator.js
@@ -1,5 +1,43 @@
 'use strict';
 
+/** @type {HTMLSelectElement} */
+const gameSelector = document.querySelector( '#booster_game_selector' );
+
+if( gameSelector )
+{
+	// Add a `div` container to display the booster pack available date
+	const availableDateContainer = document.createElement( 'div' );
+	availableDateContainer.id = 'booster_available_date';
+	gameSelector.after( availableDateContainer );
+
+	// Add an event listener to catch the details about the choosen booster pack
+	// This data is sent by `boostercreator_injected.js` when the game selector changes
+	gameSelector.addEventListener( 'boosterPackChange', function( event )
+	{
+		/** @type {CustomEvent<{ available_at_time?: string }>} */
+		const customEvent = /** @type {CustomEvent} */ ( event );
+		displayBoosterAvailableDate( customEvent.detail?.available_at_time );
+	} );
+}
+
+/**
+ * @param {string | undefined} availableDate
+ */
+function displayBoosterAvailableDate( availableDate )
+{
+	const availableDateContainer = document.getElementById( 'booster_available_date' );
+
+	if( availableDate )
+	{
+		availableDateContainer.textContent = _t( 'boostercreator_available_at_date', [ availableDate ] );
+	}
+	else
+	{
+ 		availableDateContainer.textContent = '';
+	}
+}
+
+// Inject a script into the page, so we can access page Steam variables
 const script = document.createElement( 'script' );
 script.id = 'steamdb_boostercreator';
 script.type = 'text/javascript';
--- scripts/community/boostercreator_injected.js
+++ scripts/community/boostercreator_injected.js
@@ -5,31 +5,17 @@ const gameSelector = document.querySelector( '#booster_game_selector' );
 
 if( gameSelector )
 {
-	// Add a container for the available date before the game selector
-	const availableDateContainer = document.createElement( 'div' );
-	availableDateContainer.id = 'booster_available_date';
-	gameSelector.after( availableDateContainer );
+	// Add an event listener to catch when the game selector changes and emit new rgBoosterData
+	gameSelector.addEventListener( 'change', emitBoosterAvailableDate );
 
-	// Add an event listener to update the available date when the game selector changes
-	gameSelector.addEventListener( 'change', updateBoosterAvailableDate );
-
-	// Initialize the available date when the script loads
-	updateBoosterAvailableDate();
+	// Emit rgBoosterData for current selection when the script loads
+	emitBoosterAvailableDate();
 }
 
-function updateBoosterAvailableDate( )
+function emitBoosterAvailableDate( )
 {
-	const availableDateContainer = document.getElementById( 'booster_available_date' );
 	const selectedGame = gameSelector.value;
+	const rgBoosterData = window.CBoosterCreatorPage.sm_rgBoosterData[ selectedGame ];
 
-	const availableDate = window.CBoosterCreatorPage.sm_rgBoosterData[ selectedGame ]?.available_at_time;
-
-	if( availableDate )
-	{
-		availableDateContainer.textContent = `You will not be able to create another Booster Pack for this game until ${availableDate}.`;
-	}
-	else
-	{
-		availableDateContainer.textContent = '';
-	}
+	gameSelector.dispatchEvent( new CustomEvent( 'boosterPackChange', { detail: rgBoosterData } ) );
 }
